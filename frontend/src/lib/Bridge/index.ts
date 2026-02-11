5b17c1401fd0d71b158b41a78d22e15a2c111f40
doej <doej06456@gmail.com>
2026-02-11 01:32:43 +0100

    feat: implement core logic

--- frontend/src/lib/Bridge/index.ts
+++ frontend/src/lib/Bridge/index.ts
@@ -2,3 +2,12 @@ export interface IBridge {
   id: string;
   timestamp: number;
 }
+
+export const BridgeHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as IBridge;
+  }
+};
