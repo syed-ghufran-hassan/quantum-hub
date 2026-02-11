c1dee54cd5f7f999b2d05f8e618fc1ff6866e1ca
doej <doej06456@gmail.com>
2026-02-11 01:32:21 +0100

    feat: implement core logic

--- frontend/src/lib/TestSuite/index.ts
+++ frontend/src/lib/TestSuite/index.ts
@@ -2,3 +2,12 @@ export interface ITestSuite {
   id: string;
   timestamp: number;
 }
+
+export const TestSuiteHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as ITestSuite;
+  }
+};
