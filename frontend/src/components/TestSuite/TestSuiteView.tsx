b5e01be2b33cd4f86d93cbb0345f7d4cdefc282c
doej <doej06456@gmail.com>
2026-02-11 01:32:21 +0100

    feat: implement component view

--- frontend/src/components/TestSuite/TestSuiteView.tsx
+++ frontend/src/components/TestSuite/TestSuiteView.tsx
@@ -1,1 +1,5 @@
-export const TestSuiteView = () => null;
+import React from 'react';
+
+export const TestSuiteView = () => {
+  return <div className="p-4 border rounded">Smart Contract Tests</div>;
+};
