c5dd49d59591c1a29035ce97f144d2766de075b9
doej <doej06456@gmail.com>
2026-02-11 01:32:29 +0100

    feat: implement core logic

--- frontend/src/lib/Social/index.ts
+++ frontend/src/lib/Social/index.ts
@@ -2,3 +2,12 @@ export interface ISocial {
   id: string;
   timestamp: number;
 }
+
+export const SocialHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as ISocial;
+  }
+};
