6a8a27dbd95999b4c1759e53afbe2aaffd814f58
doej <doej06456@gmail.com>
2026-02-11 01:32:50 +0100

    feat: implement core logic

--- frontend/src/lib/Vesting/index.ts
+++ frontend/src/lib/Vesting/index.ts
@@ -2,3 +2,12 @@ export interface IVesting {
   id: string;
   timestamp: number;
 }
+
+export const VestingHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as IVesting;
+  }
+};
