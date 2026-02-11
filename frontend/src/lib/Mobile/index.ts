17ee3469fa03e2b641cc75ebeb62418ec393570a
doej <doej06456@gmail.com>
2026-02-11 01:32:36 +0100

    feat: implement core logic

--- frontend/src/lib/Mobile/index.ts
+++ frontend/src/lib/Mobile/index.ts
@@ -2,3 +2,12 @@ export interface IMobile {
   id: string;
   timestamp: number;
 }
+
+export const MobileHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as IMobile;
+  }
+};
