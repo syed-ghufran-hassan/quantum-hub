a343a7ab33b8b55f4f9a38fc2135cc77c6adb7cd
doej <doej06456@gmail.com>
2026-02-11 01:32:58 +0100

    feat: implement core logic

--- frontend/src/lib/NFTLending/index.ts
+++ frontend/src/lib/NFTLending/index.ts
@@ -2,3 +2,12 @@ export interface INFTLending {
   id: string;
   timestamp: number;
 }
+
+export const NFTLendingHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as INFTLending;
+  }
+};
