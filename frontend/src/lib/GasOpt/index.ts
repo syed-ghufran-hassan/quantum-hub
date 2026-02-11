f7f8dcd9d6860407cc9de517862afa69a55e47e1
doej <doej06456@gmail.com>
2026-02-11 01:32:13 +0100

    feat: implement core logic

--- frontend/src/lib/GasOpt/index.ts
+++ frontend/src/lib/GasOpt/index.ts
@@ -2,3 +2,12 @@ export interface IGasOpt {
   id: string;
   timestamp: number;
 }
+
+export const GasOptHandler = {
+  process: (data: any) => {
+    return {
+      id: Math.random().toString(36),
+      timestamp: Date.now()
+    } as IGasOpt;
+  }
+};
